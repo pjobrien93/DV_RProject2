cc8714c9d58089f36ee5c3017be6f32f7f5c1666
Patricia O'Brien <patriciaobrien@wireless-10-147-217-124.public.utexas.edu>
2015-09-29 21:25:12 -0500

    no message

--- 02 Data Wrangling/Data_Wrangling.R
+++ 02 Data Wrangling/Data_Wrangling.R
@@ -1,5 +1,5 @@
 require("jsonlite")
 require("RCurl")
 # Change the USER and PASS below to be your UTEid
-tf <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from EMPLOYEE_SALARIES_2014"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pjo293', PASS='orcl_pjo293', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-tf
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from EMPLOYEE_SALARIES_2014"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pjo293', PASS='orcl_pjo293', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df
