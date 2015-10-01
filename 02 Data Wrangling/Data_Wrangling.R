6dea9d216874d8ae6f076ea66e4504f9fb7e9e78
Patricia O'Brien <patriciaobrien@wireless-10-147-134-140.public.utexas.edu>
2015-10-01 16:31:32 -0500

    no message

--- 02 Data Wrangling/Data_Wrangling.R
+++ 02 Data Wrangling/Data_Wrangling.R
@@ -1,5 +1,14 @@
 require("jsonlite")
 require("RCurl")
+require("dplyr")
 # Change the USER and PASS below to be your UTEid
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from EMPLOYEE_SALARIES__2014"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pjo293', PASS='orcl_pjo293', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 df
+summary(df)
+head(df)
+
+# plot average salary, group by gender
+df %>% group_by(GENDER) %>% summarize(mean(X2014_GROSS_PAY_RECEIVED)) %>% tbl_df
+
+# plot average salary group by department
+df %>% group_by(DEPARTMENT) %>% summarize(mean(CURRENT_ANNUAL_SALARY)) %>% tbl_df
